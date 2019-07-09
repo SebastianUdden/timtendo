9f96a8d9a1194784778b1dae765fea9498b3f28e
SEBSOUNdS <sebastian.udden@gmail.com>
2019-07-09 22:49:52 +0200

    Try adding hammerjs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,33 @@
 import React from "react"
+import Hammer from "hammerjs"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FaceOff from "../components/faceOff/FaceOff"
 
 const IndexPage = () => {
-  document.addEventListener(
-    "touchmove",
-    function(event) {
-      if (event.scale !== 1) {
-        event.preventDefault()
-      }
-    },
-    false
-  )
+  // the element you want to attach to, probably a wrapper for the page
+  var myElement = document.getElementById("FaceOff")
+  // create a new hammer object, setting "touchAction" ensures the user can still scroll/pan
+  var hammertime = new Hammer(myElement, {
+    prevent_default: false,
+    touchAction: "pan",
+  })
+
+  // pinch is not enabled by default in hammer
+  hammertime.get("pinch").set({
+    enable: true,
+  })
+
+  // name the events you want to capture, then call some function if you want and most importantly, add the preventDefault to block the normal pinch action
+  hammertime.on("pinch pinchend pinchstart doubletap", function(e) {
+    console.log("captured event:", e.type)
+    e.preventDefault()
+  })
 
   return (
     <Layout>
       <SEO title="Home" />
-      <FaceOff />
+      <FaceOff id="FaceOff" />
     </Layout>
   )
 }
